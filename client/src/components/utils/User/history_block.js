f06793fce3ce23b206b519baba91c4f676f791f9
Rehman Ali <rehman.vectorcoder@gmail.com>
2020-03-29 21:13:50 +0500

    'complete-mern-stack-web-app'

---
+++ client/src/components/utils/User/history_block.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+// {moment(product.dateOfPurchase).format("MM-DD-YYYY")}
+
+const UserHistoryBlock = props => {
+  const renderBlocks = () =>
+    props.products
+      ? props.products.map((product, i) => (
+          <tr key={i}>
+            <td>{product.porder}</td>
+            <td>
+              {product.brand} {product.name}
+            </td>
+            <td>$ {product.price}</td>
+            <td>{product.quantity}</td>
+          </tr>
+        ))
+      : null;
+
+  return (
+    <div className="history_blocks">
+      <table>
+        <thead>
+          <tr>
+            <th>Order number</th>
+            <th>Product</th>
+            <th>Price paid</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>{renderBlocks()}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserHistoryBlock;
